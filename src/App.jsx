5277f6fad206e47f52564be0193d52656cac253b
Facundo Rivero <facundoyamilrivero@gmail.com>
2026-02-27 16:26:26 -0300

    Corrigiendo mayusculas de Hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
 import AppRoutes from "./routes/AppRoutes";
 import Footer from "./components/footer";
-import Hero from "./components/hero";
+import Hero from "./components/Hero";
 import "./App.css";
 
 
