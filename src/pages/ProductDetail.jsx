e0d94cfbd1954eab77bd007790d1813a5bf2f3a3
Facundo Rivero <facundoyamilrivero@gmail.com>
2026-02-27 16:57:17 -0300

    mas problemas de mayusculas

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,7 +1,7 @@
 import {  useEffect, useState } from "react";    
 import {  useParams } from "react-router-dom";   
 import axios from "axios";
-import "../styles/ProductDetail.css"
+import "../styles/productDetail.css"
 
 const ProductDetail = () => {
 
